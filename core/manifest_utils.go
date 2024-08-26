919db8c1e46521077cc01cffc5ee210157be911a
Taylor Monacelli <taylormonacelli@gmail.com>
2024-08-26 15:19:49 -0400

    Remove unused methods

--- core/manifest_utils.go
+++ core/manifest_utils.go
@@ -1,7 +1,6 @@
 package core
 
 import (
-	"fmt"
 	"path/filepath"
 	"strings"
 
@@ -24,60 +23,4 @@ func normalizePathForComparison(path string) (string, error) {
 	cleanPath = strings.TrimPrefix(cleanPath, "/private")
 
 	return cleanPath, nil
-}
-
-func (mg *ManifestGenerator) CommentItem(manifestFile, item string) error {
-	manifest, err := mg.ReadManifest(manifestFile)
-	if err != nil {
-		return err
-	}
-
-	normalizedItem, err := normalizePathForComparison(item)
-	if err != nil {
-		return err
-	}
-	if _, exists := manifest.FileList[normalizedItem]; exists {
-		manifest.FileList[normalizedItem] = true
-		return mg.WriteManifest(manifest, manifestFile)
-	}
-
-	return fmt.Errorf("item not found in manifest")
-}
-
-func (mg *ManifestGenerator) UncommentItem(manifestFile, item string) error {
-	manifest, err := mg.ReadManifest(manifestFile)
-	if err != nil {
-		return err
-	}
-
-	normalizedItem, err := normalizePathForComparison(item)
-	if err != nil {
-		return err
-	}
-	if _, exists := manifest.FileList[normalizedItem]; exists {
-		manifest.FileList[normalizedItem] = false
-		return mg.WriteManifest(manifest, manifestFile)
-	}
-
-	return fmt.Errorf("item not found in manifest")
-}
-
-func (mg *ManifestGenerator) GetItemStatus(manifestFile, item string) (string, error) {
-	manifest, err := mg.ReadManifest(manifestFile)
-	if err != nil {
-		return "", err
-	}
-
-	normalizedItem, err := normalizePathForComparison(item)
-	if err != nil {
-		return "", err
-	}
-	if isCommented, exists := manifest.FileList[normalizedItem]; exists {
-		if isCommented {
-			return "disabled", nil
-		}
-		return "enabled", nil
-	}
-
-	return "not in list", nil
 }
